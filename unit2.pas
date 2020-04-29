e0418f33fc0b703174c4d4c7cc9cf328424e86d0
fadli <ftobiansyah@gmail.com>
2020-04-29 11:08:48 +0700

    Operasi Tipe Data

--- unit2.pas
+++ unit2.pas
@@ -32,8 +32,32 @@ implementation
 { TForm1 }
 
 procedure TForm1.Button1Click(Sender: TObject);
+var
+  namaDepan: String;
+  namaTengah, namaBelakang: String;
+  usia: Integer;
+  targetKuliah: Integer;
+  IPK: Double;
+  nilaiAbjad: Char;
+  tampan: boolean;
 begin
-  showMessage('hello'+eNama.text);
+  namaDepan:='Fadli';
+  namaBelakang:='Tobiansyah';
+  usia:=22;
+  IPK:=3.8482462792;
+  tampan:=False;
+
+
+ MessageDlg('Judul Pesan',namaDepan + ' '+namaTengah + ' '+namaBelakang,mtInformation,[mboK],0);
+
+ MessageDlg('usia',intToStr(usia),mtInformation,[mboK],0);
+
+ MessageDlg('IPK',FloatToStrF(IPK,ffFixed,3,2),mtInformation,[mboK],0);
+
+ MessageDlg('Tampan',BooltoStr(Tampan),mtInformation,[mboK],0);
+
+ MessageDlg('Hari Ini',FormatDateTime('dddd, dd-MM-yyyy',now),
+         mtInformation,[mboK],0);
 end;
 
 end.
