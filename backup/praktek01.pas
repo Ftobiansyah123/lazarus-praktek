b32c1387c5ca8e4b3c05dd6c4a5d301e759ab6c6
fadli <ftobiansyah@gmail.com>
2020-04-29 09:47:58 +0700

    membuat show message hello world

---
+++ backup/praktek01.pas
@@ -0,0 +1,39 @@
+unit praktek01;
+
+{$mode objfpc}{$H+}
+
+interface
+
+uses
+  Classes, SysUtils, FileUtil, Forms, Controls, Graphics, Dialogs, Menus,
+  StdCtrls;
+
+type
+
+  { TForm1 }
+
+  TForm1 = class(TForm)
+    Button1: TButton;
+    procedure Button1Click(Sender: TObject);
+  private
+
+  public
+
+  end;
+
+var
+  Form1: TForm1;
+
+implementation
+
+{$R *.lfm}
+
+{ TForm1 }
+
+procedure TForm1.Button1Click(Sender: TObject);
+begin
+
+end;
+
+end.
+
